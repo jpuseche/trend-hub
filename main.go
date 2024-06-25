5656cf91c0a22f60d5bca2ca1aec6d63f094d549
[juseche] <[juseche@motus.com]>
2024-06-25 17:58:57 -0500

    feat(headless browser): tiktok scrape

--- main.go
+++ main.go
@@ -15,33 +15,51 @@ type Trend struct {
 	Author      string
 }
 
-var urls = []string{"google.com", "youtube.com", "twitter.com"}
+// This type defines the scraping options to be used per website
+type WebScrapeConfig struct {
+	URL        string
+	QueryClass string
+}
+
+// This saves the group of website configs to use on the web scraping
+var wscGroup = []WebScrapeConfig{
+	{
+		URL:        "https://www.tiktok.com/discover/tech-trends",
+		QueryClass: ".css-1anth1x-DivVideoDescription",
+	},
+}
 
 func home(res http.ResponseWriter, req *http.Request) {
-	scraper.ScrapeData()
-
 	startTime := time.Now()
 	fmt.Println("Getting Trends")
 
-	trendsChannel := make(chan Trend)
+	trendsChannel := make(chan []Trend)
 
-	for _, url := range urls {
+	// var wg sync.WaitGroup
+	for _, wsc := range wscGroup {
+		// wg.Add(1)
+
 		go func(keyword string, website string) {
-			time.Sleep(2 * time.Second)
-
-			newTrend := Trend{Title: "some Topic", Description: "this descripition", Website: url, Author: "this Author"}
-			trendsChannel <- newTrend
-		}("tech", url)
+			titles := scraper.ScrapeTitles(wsc.URL, wsc.QueryClass)
+			trends := []Trend{}
+			for _, title := range titles {
+				newTrend := Trend{Title: title, Description: "this descripition", Website: wsc.URL, Author: "this Author"}
+				trends = append(trends, newTrend)
+			}
+			trendsChannel <- trends
+		}("tech", wsc.URL)
 	}
 
+	// wg.Wait()
 	trends := []Trend{}
-	for range urls {
-		trends = append(trends, <-trendsChannel)
+	for range wscGroup {
+		trends = append(trends, <-trendsChannel...)
 	}
 	endTime := time.Now()
 
 	fmt.Printf("Got Trends sucessfully\n")
 	fmt.Printf("Duration: %v\n", endTime.Sub(startTime))
+	fmt.Printf("Trends Amount: %v\n", len(trends))
 	fmt.Fprint(res, trends)
 }
 
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,25 +1,42 @@
 package scraper
 
 import (
+	"context"
 	"fmt"
+	"log"
 
-	"github.com/gocolly/colly"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 )
 
-func ScrapeData() {
+func ScrapeTitles(url string, queryClass string) []string {
 	fmt.Println("Started scraping")
 
-	c := colly.NewCollector()
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
 
-	c.OnHTML(".body_bold.color-titles", func(e *colly.HTMLElement) {
-		fmt.Println("---------------------------------")
-		fmt.Println(e.Text)
-	})
-
-	scrapingUrl := "https://brightdata.com/"
-	err := c.Visit(scrapingUrl)
+	// Run tasks
+	// Replace "http://example.com" with the URL you want to scrape
+	var nodes []*cdp.Node
+	err := chromedp.Run(ctx,
+		// Navigate to site
+		chromedp.Navigate(url),
+		// Wait for an element that is rendered by JavaScript
+		chromedp.WaitVisible(queryClass, chromedp.ByQueryAll),
+		// Retrieve the content of the element
+		chromedp.Nodes(queryClass, &nodes, chromedp.ByQueryAll),
+	)
 	if err != nil {
-		fmt.Printf("Can't get url for scraping: %v\n", scrapingUrl)
+		log.Fatal(err)
 	}
 
+	// Do something with the extracted content
+	fmt.Println("Printing Response Nodes")
+
+	titles := []string{}
+	for _, node := range nodes {
+		titles = append(titles, node.Children[0].NodeValue)
+	}
+
+	return titles
 }
